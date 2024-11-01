d017604de86a14b299f8453f9eb621aea62b2410
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2024-11-01 14:02:09 +0530

    add some animations

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import toast from "react-hot-toast";
+import { motion } from "framer-motion";
 
 function SignUp() {
   const [agree, setAgree] = useState(false);
@@ -82,161 +83,172 @@ function SignUp() {
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen p-4 ">
-      <div className="max-w-md w-full mx-auto border border-gray-300 rounded-2xl p-8 bg-white shadow-lg">
-        <div className="text-center mb-12">
-          <a href="javascript:void(0)">
-            <img
-              src="https://readymadeui.com/readymadeui.svg"
-              alt="logo"
-              className="w-40 inline-block"
-            />
-          </a>
-        </div>
+      <motion.div
+        className="w-full"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.9 }}
+      >
+        <div className="max-w-md w-full mx-auto border border-gray-300 rounded-2xl p-8 bg-white shadow-lg">
+          <div className="text-center mb-12">
+            <a href="javascript:void(0)">
+              <img
+                src="https://readymadeui.com/readymadeui.svg"
+                alt="logo"
+                className="w-40 inline-block"
+              />
+            </a>
+          </div>
 
-        <form>
-          <div className="space-y-2">
-            <div className="relative">
-              <label className="text-gray-800 text-sm mb-2 block">Email</label>
-              <input
-                name="email"
-                type="text"
-                className={`text-gray-800 bg-white border ${
-                  validation.email === null
-                    ? "border-gray-300"
-                    : validation.email
-                    ? "border-green-500"
-                    : "border-red-500"
-                } w-full text-sm px-4 py-3 rounded-md outline-blue-500`}
-                placeholder="Enter email"
-                onChange={handleInputChange}
-              />
-              {validation.email !== null && (
-                <span
-                  className={`absolute right-3 top-10 transform -translate-y-1/2 `}
+          <form>
+            <div className="space-y-2">
+              <div className="relative">
+                <label className="text-gray-800 text-sm mb-2 block">
+                  Email
+                </label>
+                <input
+                  name="email"
+                  type="text"
+                  className={`text-gray-800 bg-white border ${
+                    validation.email === null
+                      ? "border-gray-300"
+                      : validation.email
+                      ? "border-green-500"
+                      : "border-red-500"
+                  } w-full text-sm px-4 py-3 rounded-md outline-blue-500`}
+                  placeholder="Enter email"
+                  onChange={handleInputChange}
+                />
+                {validation.email !== null && (
+                  <span
+                    className={`absolute right-3 top-10 transform -translate-y-1/2 `}
+                  >
+                    {validation.email ? "✅" : "❌"}
+                  </span>
+                )}
+              </div>
+              <div className="relative">
+                <label className="text-gray-800 text-sm mb-2 block">
+                  Username
+                </label>
+                <input
+                  name="name"
+                  type="text"
+                  className={`text-gray-800 bg-white border ${
+                    validation.name === null
+                      ? "border-gray-300"
+                      : validation.name
+                      ? "border-green-500"
+                      : "border-red-500"
+                  } w-full text-sm px-4 py-3 rounded-md outline-blue-500`}
+                  placeholder="Enter Username"
+                  onChange={handleInputChange}
+                />
+                {validation.name !== null && (
+                  <span
+                    className={`absolute right-3 top-10 transform -translate-y-1/2 `}
+                  >
+                    {validation.name ? "✅" : "❌"}
+                  </span>
+                )}
+              </div>
+              <div className="relative">
+                <label className="text-gray-800 text-sm mb-2 block">
+                  Password
+                </label>
+                <input
+                  name="password"
+                  type="password"
+                  className={`text-gray-800 bg-white border ${
+                    validation.password === null
+                      ? "border-gray-300"
+                      : validation.password
+                      ? "border-green-500"
+                      : "border-red-500"
+                  } w-full text-sm px-4 py-3 rounded-md outline-blue-500`}
+                  placeholder="Enter password"
+                  onChange={handleInputChange}
+                />
+                {validation.password !== null && (
+                  <span
+                    className={`absolute right-3 top-10 transform -translate-y-1/2 `}
+                  >
+                    {validation.password ? "✅" : "❌"}
+                  </span>
+                )}
+              </div>
+              <div className="relative">
+                <label className="text-gray-800 text-sm mb-2 block">
+                  Confirm Password
+                </label>
+                <input
+                  name="cpassword"
+                  type="password"
+                  className={`text-gray-800 bg-white border ${
+                    validation.cpassword === null
+                      ? "border-gray-300"
+                      : validation.cpassword
+                      ? "border-green-500"
+                      : "border-red-500"
+                  } w-full text-sm px-4 py-3 rounded-md outline-blue-500`}
+                  placeholder="Enter confirm password"
+                  onChange={handleInputChange}
+                />
+                {validation.cpassword !== null && (
+                  <span
+                    className={`absolute right-3 top-10 transform -translate-y-1/2 `}
+                  >
+                    {validation.cpassword ? "✅" : "❌"}
+                  </span>
+                )}
+              </div>
+
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
+                  onClick={handleAgree}
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="text-gray-800 ml-3 block text-sm"
                 >
-                  {validation.email ? "✅" : "❌"}
-                </span>
-              )}
-            </div>
-            <div className="relative">
-              <label className="text-gray-800 text-sm mb-2 block">
-                Username
-              </label>
-              <input
-                name="name"
-                type="text"
-                className={`text-gray-800 bg-white border ${
-                  validation.name === null
-                    ? "border-gray-300"
-                    : validation.name
-                    ? "border-green-500"
-                    : "border-red-500"
-                } w-full text-sm px-4 py-3 rounded-md outline-blue-500`}
-                placeholder="Enter Username"
-                onChange={handleInputChange}
-              />
-              {validation.name !== null && (
-                <span
-                  className={`absolute right-3 top-10 transform -translate-y-1/2 `}
-                >
-                  {validation.name ? "✅" : "❌"}
-                </span>
-              )}
-            </div>
-            <div className="relative">
-              <label className="text-gray-800 text-sm mb-2 block">
-                Password
-              </label>
-              <input
-                name="password"
-                type="password"
-                className={`text-gray-800 bg-white border ${
-                  validation.password === null
-                    ? "border-gray-300"
-                    : validation.password
-                    ? "border-green-500"
-                    : "border-red-500"
-                } w-full text-sm px-4 py-3 rounded-md outline-blue-500`}
-                placeholder="Enter password"
-                onChange={handleInputChange}
-              />
-              {validation.password !== null && (
-                <span
-                  className={`absolute right-3 top-10 transform -translate-y-1/2 `}
-                >
-                  {validation.password ? "✅" : "❌"}
-                </span>
-              )}
-            </div>
-            <div className="relative">
-              <label className="text-gray-800 text-sm mb-2 block">
-                Confirm Password
-              </label>
-              <input
-                name="cpassword"
-                type="password"
-                className={`text-gray-800 bg-white border ${
-                  validation.cpassword === null
-                    ? "border-gray-300"
-                    : validation.cpassword
-                    ? "border-green-500"
-                    : "border-red-500"
-                } w-full text-sm px-4 py-3 rounded-md outline-blue-500`}
-                placeholder="Enter confirm password"
-                onChange={handleInputChange}
-              />
-              {validation.cpassword !== null && (
-                <span
-                  className={`absolute right-3 top-10 transform -translate-y-1/2 `}
-                >
-                  {validation.cpassword ? "✅" : "❌"}
-                </span>
-              )}
+                  I accept the{" "}
+                  <Link
+                    to="/terms-and-conditions"
+                    className="text-blue-600 font-semibold hover:underline ml-1"
+                  >
+                    Terms and Conditions
+                  </Link>
+                </label>
+              </div>
             </div>
 
-            <div className="flex items-center">
-              <input
-                id="remember-me"
-                name="remember-me"
-                type="checkbox"
-                className="h-4 w-4 shrink-0 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
-                onClick={handleAgree}
-              />
-              <label
-                htmlFor="remember-me"
-                className="text-gray-800 ml-3 block text-sm"
+            <div className="mt-12">
+              <motion.button
+                type="button"
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                className="w-full py-3 px-4 text-sm tracking-wider font-semibold rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
+                onClick={handleSubmit}
               >
-                I accept the{" "}
-                <Link
-                  to="/terms-and-conditions"
-                  className="text-blue-600 font-semibold hover:underline ml-1"
-                >
-                  Terms and Conditions
-                </Link>
-              </label>
+                Create an account
+              </motion.button>
             </div>
-          </div>
-
-          <div className="mt-12">
-            <button
-              type="button"
-              className="w-full py-3 px-4 text-sm tracking-wider font-semibold rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none"
-              onClick={handleSubmit}
-            >
-              Create an account
-            </button>
-          </div>
-          <p className="text-gray-800 text-sm mt-6 text-center">
-            Already have an account?{" "}
-            <Link
-              to="/login"
-              className="text-blue-600 font-semibold hover:underline ml-1"
-            >
-              Login here
-            </Link>
-          </p>
-        </form>
-      </div>
+            <p className="text-gray-800 text-sm mt-6 text-center">
+              Already have an account?{" "}
+              <Link
+                to="/login"
+                className="text-blue-600 font-semibold hover:underline ml-1"
+              >
+                Login here
+              </Link>
+            </p>
+          </form>
+        </div>
+      </motion.div>
     </div>
   );
 }
