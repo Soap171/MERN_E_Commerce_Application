c36cadfa349325b595d3eaad57f2a896e2ac4bd7
Sachinthana Jayathunga <145235176+Soap171@users.noreply.github.com>
2024-11-11 09:03:23 +0530

    add google auth in backend

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -261,7 +261,55 @@ export const resetPassword = async (req, res, next) => {
     next(errorHandler(500, "Internal server error"));
   }
 };
-export const google = async (req, res) => {};
+export const google = async (req, res, next) => {
+  const { email, name, googlePhotoUrl } = req.body;
+  if (!email || !name)
+    return next(errorHandler(400, "All fields are required"));
+
+  try {
+    const user = User.findOne({ email });
+
+    if (user) {
+      const { refreshToken, accessToken } = generateTokens(user._id);
+      await storeRefreshToken(user._id, refreshToken, next);
+      setCookies(res, accessToken, refreshToken, next);
+
+      res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        isVerified: user.isVerified,
+      });
+    } else {
+      const token = createVerifyToken();
+
+      const user = await User.create({
+        name,
+        email,
+        password,
+        verificationToken: token,
+        profilePicture: googlePhotoUrl,
+        verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
+      });
+
+      const { accessToken, refreshToken } = generateTokens(user._id);
+      await storeRefreshToken(user._id, refreshToken, next);
+      setCookies(res, accessToken, refreshToken, next);
+      await sendVerificationEmail(email, token, next);
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        isVerified: user.isVerified,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    next(errorHandler(500, "Internal server error"));
+  }
+};
 
 // profile
 export const getProfile = async (req, res, next) => {};
